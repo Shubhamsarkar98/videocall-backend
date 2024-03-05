1ae7d836052e1718dea35dc36c9e556766980b06
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-03-05 12:22:58 +0530

    add comment

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from "express";
 import cors from 'cors'
 import cookieParser from "cookie-parser";
 const app = express()
-
+//cors add in express 
 app.use(cors({
     origin: process.env.CORS_Origin,
     credentials: true
 }))
-
+//app json
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended }))
 app.use(express.static("public"))
