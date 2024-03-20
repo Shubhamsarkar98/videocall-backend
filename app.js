1c2d82d05077e1ff8cf3aa0da05ef6003ddcdf3e
shubhamsarkar9435 <shubhamsarkar9435@gmail.com>
2024-03-20 20:50:27 +0530

    app add

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(cors({
     origin: process.env.CORS_Origin,
     credentials: true
 }))
+console.log(app)
 //app json
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended }))
